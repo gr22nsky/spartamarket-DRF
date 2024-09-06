232e9dde70fe20e13c9f97207cc8d761e42f9153
gr22nsky <gr2nsky@gmail.com>
2024-09-06 10:29:21 +0900

    products_CRUD

--- products/urls.py
+++ products/urls.py
@@ -5,5 +5,6 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('', views.ProductListView.as_view())
+    path('', views.ProductListView.as_view(), name='product_list'),
+    path('<int:pk>/', views.ProductDetailView.as_view(), name='product_detail'),
 ]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- products/views.py
+++ products/views.py
@@ -1,6 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 from .models import Product
+from .serializers import ProductSerializer
 
 class ProductListView(APIView):
     
@@ -9,9 +11,34 @@ class ProductListView(APIView):
         content = request.data.get('content')
         image = request.data.get('image')
         product = Product.objects.create(title=title, content=content, image=image)
-        res_json = {
-            'title' : product.title,
-            'content' : product.content,
-            'imgae' : product.image.name
-        }
-        return Response(res_json, status = 201)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    
+    def get(self, request):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+    
+
+class ProductDetailView(APIView):
+    def get_object(self, request, pk):
+        return get_object_or_404(Product, pk=pk)
+    
+    def get(self, request, pk):
+        product = self.get_object(request, pk)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    
+    def put(self, request, pk):
+        product = self.get_object(request, pk)
+        serializer = ProductSerializer(product, data=request.data, partial=True)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+
+    def delete(self, request, pk):
+        product = self.get_object(request, pk)
+        product.delete()
+        message = {'글이 삭제되었습니다.'}
+        return Response(message)
+        
